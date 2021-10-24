0ace90cbffbeb0453c8fe44f85234aac8392a99e
Adam Kakes <adamkakes@gmail.com>
2021-10-24 12:13:03 +0200

    final

--- vetsi_mensi/vetsi_mensi.cpp
+++ vetsi_mensi/vetsi_mensi.cpp
@@ -1,14 +1,14 @@
 ﻿#include <stdio.h>
 
 int main(void) {
-	int a = 3, b = 2;
+	int a = 5, b = 3;
 	if (a > b) {
 		printf("Cislo %d je vetsi nez cislo %d\n", a, b);
 	}
 	if (b > a)
-		{
-			printf("Cislo %d je vetsi nez cislo %d\n", b, a);
-		}
+	{
+		printf("Cislo %d je vetsi nez cislo %d\n", b, a);
+	}
 	if (a == b)
 	{
 		printf("Cislo %d se rovna cislu %d\n", a, b);
